0b563b40def2e8d3ef7be6d00b9adf8389826922
Masayoshi Tamura <muramasa8191@gmail.com>
2019-06-03 15:08:15 +0900

    test

--- test/numex_test.exs
+++ test/numex_test.exs
@@ -2,7 +2,7 @@ defmodule NumexTest do
   use ExUnit.Case
   doctest NumEx
 
-  test "greets the world" do
-    assert NumEx.hello() == :world
+  test "add test" do
+    assert NumEx.add([1, 2], [3, 4]) == [4, 6]
   end
 end
